cfa3dd5f5f59fc5d5d294e5de8cdd1f31ba5e143
Frank Adler <frank@adfr.io>
2022-03-02 21:51:28 +0100

    :recycle: log to syslog, lower temperature level to 40 deg

--- heat.sh
+++ heat.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-while endless=0; do 
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+while true; do
     temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
     echo "Current CPU temperature: $temp"
-    
-    if [$temp < 50]; then
-        echo "Too cold, heating up for 10min."
-        stress-ng --cpu 0 --matrix-size 256 --tz  -t 600
+
+    if (( $(echo "$temp < 40" | bc -l) )); then
+        echo "Too cold, heating up for 10min..."
+        stress-ng --cpu 3 --matrix-size 256 --tz  -t 600 &> /dev/null
     else
-        echo "Warm enough, sleeping for 2min."
-        sleep 120;
+        echo "Warm enough, sleeping for 1min."
+        sleep 60;
     fi
 done
